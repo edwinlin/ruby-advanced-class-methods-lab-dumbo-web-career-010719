d48d5e9fff7c58e74dbfb6b1ed4a95aefb6426cf
Edwin Lin <edwinlin@gmail.com>
2019-01-10 18:13:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,42 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    self.all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort {|x,y| x.name <=> y.name }
+  end
+  
+  def self.new_from_filename(filename)
+
+  end
+  
 end
